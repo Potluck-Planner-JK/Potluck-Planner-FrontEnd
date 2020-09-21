0c292be02850f8776d54b1c240a6353e06bb9125
Mason Davis <masondavis2009@gmail.com>
2020-09-21 17:55:20 -0500

    Progress being made

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PrivateRoute from "./Utils/PrivateRoute"
+import Login from "./Components/Login"
+import { Router, Switch, Route } from "react-router-dom"
+import Register from "./Components/Register"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <h1>Potluck Planner</h1>
+        <Login />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
